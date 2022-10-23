9a2ed351f201c5d3e450ca285c29b8c8d83ab5e2
Pavel Lepikhov <lepikhovpv@gmail.com>
2022-10-23 23:08:41 +0300

    Initial commit

---
+++ app/processing.py
@@ -0,0 +1,21 @@
+import io
+import os
+import string
+import time
+
+#import numpy as np
+#import tensorflow as tf
+#from PIL import Image
+
+
+def prepare_image(img_bytes):
+    pass
+
+def predict_result(img):
+    keypoints = [
+        [1, 2],
+        [3, 4],
+        [5, 6]
+    ]
+
+    return keypoints
---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+
+from app import app
+from app.processing import predict_result, prepare_image
+
+
+@app.route('/predict', methods=['POST'])
+def infer_image():
+    # Catch the image file from a POST request
+    if 'file' not in request.files:
+        return "Please try again. The Image doesn't exist"
+    
+    file = request.files.get('file')
+
+    if not file:
+        return
+
+    # Read the image
+    img_bytes = file.read()
+
+    # Prepare the image
+    img = prepare_image(img_bytes)
+
+    # Return on a JSON format
+    return jsonify(prediction=predict_result(img))
+    
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Service for automatic determination of the key points of the horse'
